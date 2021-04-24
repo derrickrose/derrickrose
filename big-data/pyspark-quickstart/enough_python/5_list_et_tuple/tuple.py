2b9a3a18f8958ef91fa3e979e628181f40027887
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-04-25 00:15:22 +0200

    update code

--- big-data/pyspark-quickstart/enough_python/5_list_et_tuple/tuple.py
+++ big-data/pyspark-quickstart/enough_python/5_list_et_tuple/tuple.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-""" methode qui retourne un tuple composer de la valeur entiere
-et du reste de la division
-"""
 
 
 def decomposer(nombre, dividande):
-    """ definition de la methode main """
+    """ methode qui retourne un tuple composer de la valeur entiere
+    et du reste de la division
+    """
+    return int(nombre / dividande), nombre % dividande
 
 
 if __name__ == "__main__":
+    """ definition de la methode main """
+
     """ definition dun tuple """
+    my_tuple = decomposer(10, 3)
+    print(my_tuple)
 
     """ interchanger un tuple """
+    a, b = 22, 5
+    print("before interchange", a)
+    a, b = b, a
+    print("after interchange", a)
+    a, b = 10, 10
+    print("after change value", a)
+    my_tuple = (a, b)
+    print(my_tuple)
