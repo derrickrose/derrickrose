0e3a438510ec46f5546e318603404eec275b7be0
derrickrose <randofrils@gmail.com>
2021-10-25 23:47:40 +0200

    add jdk

--- dev-sec-ops/docker/04_script_to_delete_all/delete.sh
+++ dev-sec-ops/docker/04_script_to_delete_all/delete.sh
@@ -8,19 +8,23 @@ delete_images() {
   docker rmi -f $(docker images -aq)
 }
 
+delete_volumes() {
+  docker volume rm $(docker volume ls -q)
+}
+
 purge_all() {
   docker system prune -af
 }
-
 
 if [ -z $1 ]; then
   stop_running_containers
   delete_images
   purge_all
+  delete_volumes
 elif [ $1 == "c" ] || [ $1 == "containers" ]; then
   stop_running_containers
 elif [ $1 == "i" ] || [ $1 == "images" ]; then
   delete_images
+elif [ $1 == "v" ] || [ $1 == "volumes" ]; then
+  delete_volumes
 fi
-
-
