62ccb47a673e2a9b5c15ea1d135089a652b5dcfe
Andrianarisoa Ampilahy <derrickrose>
2023-06-12 23:05:13 +0200

    update code

--- ops/docker/00_acloud_course/06_dockerize_a_flask_app/notes/config.py
+++ ops/docker/00_acloud_course/06_dockerize_a_flask_app/notes/config.py
@@ -1,9 +1,9 @@
 import os
 
-db_host = os.environ.get('DB_HOST', default='localhost')
+db_host = os.environ.get('DB_HOST', default='postgres_db')
 db_name = os.environ.get('DB_NAME', default='notes')
-db_user = os.environ.get('DB_USERNAME', default='notes')
-db_password = os.environ.get('DB_PASSWORD', default='')
+db_user = os.environ.get('DB_USERNAME', default='postgres')
+db_password = os.environ.get('DB_PASSWORD', default='mysecretpassword')  # replace by correct password
 db_port = os.environ.get('DB_PORT', default='5432')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
