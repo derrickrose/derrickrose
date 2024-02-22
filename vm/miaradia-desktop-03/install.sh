f0a589f8b184b1b0a2af2367c0da024e733af9e1
Andrianarisoa Ampilahy <derrickrose>
2024-02-22 21:29:14 +0100

    update vm

--- vm/miaradia-desktop-03/install.sh
+++ vm/miaradia-desktop-03/install.sh
@@ -1,8 +1,6 @@
 sudo apt update
 sudo apt install net-tools
 sudo apt install vim
-sudo apt install openssl
-sudo apt install curl
-sudo apt install nginx
 sudo apt install openssh-server
+sudo apt install curl
 sudo apt install python3-certbot-nginx
