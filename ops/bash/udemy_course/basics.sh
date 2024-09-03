6483fb58cefa3c7fbc9eeedb138078df75668245
frils <randofrils@gmail.com>
2024-09-03 19:43:38 +0200

    update basics

--- ops/bash/udemy_course/basics.sh
+++ ops/bash/udemy_course/basics.sh
@@ -16,5 +16,47 @@ uname -a
 who
 whoami
 
+# list content of a dir
+ls
+
+# create a dir
+mkdir test_baba
+
+# change current directory
+cd test_baba
+
+# print the path of current directory
+pwd
+
+# create a file
+touch test_file
+echo "aaa" > test_file #add text in a file but also create it if doesnt exist
+date +%d-%m-%Y >> test_file # print the formatted date and redirect it at the end of file
+cat test_file # print content of the file
+
+# copy a file
+cp test_file test_file_copy
+cat test_file_copy
+ls
+
+# move file (rename or here overwrite the existing)
+mv test_file_copy test_file
+ls # there should be only one file left
+
+# delete a file
+rm test_file
+
+# change directory get back to the parent
+cd ..
+
+# remove directory
+rmdir test_baba
+
+# cut subsequence of words from a text , start from index 1
+echo "baba bobo baba" | cut -c 6-10
+
+
+
+
 
 
